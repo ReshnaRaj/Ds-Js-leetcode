ca6089858f30367e1fc385c93e68d161c7fe8e7c
Reshna K P <116788191+ReshnaRaj@users.noreply.github.com>
2023-07-18 00:17:49 +0530

    Update duplicate.js

--- duplicate.js
+++ duplicate.js
@@ -1,3 +1,5 @@
+find the duplicate array elements 
+-------------------------------------------
 const array=[1,2,3,4,5,1,2,3]
 for(let i=0;i<array.length;i++){
     let count=0
@@ -13,3 +15,23 @@ for(let i=0;i<array.length;i++){
         }
     
 }
+
+find the unique array elements
+----------------------------------------
+ const array=[1,2,3,4,5,1,2,3]
+for(let i=0;i<array.length;i++){
+    let count=0
+    for(let j=i+1;j<array.length;j++){
+        if(array[i]===array[j]){
+            array[j]=null
+            count++
+          
+        }
+    }
+        if(count==0 && array[i] !== null){
+        console.log(array[i])
+        }
+    
+}
+
+
