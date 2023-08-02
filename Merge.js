96fb69d731a4064543ccb1142dc15280836598fd
Reshna K P <116788191+ReshnaRaj@users.noreply.github.com>
2023-08-02 17:06:13 +0530

    Update Merge.js

--- Merge.js
+++ Merge.js
@@ -11,3 +11,15 @@ mergedArray[array1.length+i]=array2[i]
 return mergedArray
 }
 console.log(merge([1,2,3],[4,5,6]))
+// linear complexity
+function merge(array1,array2){
+let mergedArray=array1
+ 
+let n=mergedArray.length
+for(let i=0;i<array2.length;i++)
+{
+mergedArray[n+i]=array2[i]
+}
+return mergedArray
+}
+console.log(merge([1,2,3,3],[4,5,6,7]))
