4e3e6f57f82037277a127f4a97ddfcc1f22c340a
Reshna K P <116788191+ReshnaRaj@users.noreply.github.com>
2023-08-06 14:27:17 +0530

    Create Get name initials.js

---
+++ Get name initials.js
@@ -0,0 +1,8 @@
+const input="gram Hhj hbb bdf bdfhsiu bdfsb"
+const in=input.split(" ").map((word)=>word[0]).join(" ")
+console.log(in) g H h b b b
+
+
+  const input = "George Raymond Richard Martin";
+const inp=input.split(" ").map((word)=>word[0]).join(" ")
+console.log(inp) G R R M
