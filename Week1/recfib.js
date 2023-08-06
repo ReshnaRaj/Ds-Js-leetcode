949e6d775a3c9a67e10edea65e33856478aab2c3
Reshna K P <116788191+ReshnaRaj@users.noreply.github.com>
2023-08-06 17:26:46 +0530

    Update recfib.js

--- Week1/recfib.js
+++ Week1/recfib.js
@@ -1,10 +1,2 @@
-function fibonacci(n) {
-    if (n <= 1) {   // base case
-      return n;
-    } else {
-      return fibonacci(n - 1) + fibonacci(n - 2);   // recursive case
-    }
-  }
-  const result = fibonacci(3);
-  console.log(result);   // Output: 13
+ 
     
