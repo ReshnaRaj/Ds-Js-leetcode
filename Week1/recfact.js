b929ac3b7a8f1fd495acf3563ea9b6a682bd7abb
Reshna K P <116788191+ReshnaRaj@users.noreply.github.com>
2023-08-07 10:09:04 +0530

    Update recfact.js

--- Week1/recfact.js
+++ Week1/recfact.js
@@ -9,3 +9,24 @@ function factorial(n) {
   console.log(result);   // Output: 120
   
 // console.log(factorial(5))  
+
+
+// normal case
+function factorial(number) {
+    if (number < 0) {
+      return 'Error! Factorial for negative number does not exist.';
+    } else if (number === 0) {
+      return `The factorial of ${number} is 1.`;
+    } else {
+      let fact = 1;
+      for (let i = 1; i <= number; i++) {
+        fact *= i;
+      }
+      return `The factorial of ${number} is ${fact}.`;
+    }
+  }
+  
+   
+  const result = factorial(6);
+  console.log(result);
+  
