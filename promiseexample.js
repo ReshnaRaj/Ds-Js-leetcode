4c45c93b573f4f5057a7ea625efb16fdb97f8e58
Reshna K P <116788191+ReshnaRaj@users.noreply.github.com>
2023-08-12 16:59:09 +0530

    promise

---
+++ promiseexample.js
@@ -0,0 +1,21 @@
+function hello(a){
+    return new Promise((resolve,reject)=>{
+        if(a>5){
+            resolve({state:"success"})
+        }else{
+            reject({state:"failed"})
+        }
+    }) .then((result)=>{
+         console.log(result.state)
+    }).catch((err)=>{
+          console.log(err.state)
+  })
+  }
+  let a=7
+  hello(a)
+  // hello(a).then((result)=>{
+  //     console.log(result.state)
+  // }).catch((err)=>{
+  //         console.log(err.state)
+  // })
+  
